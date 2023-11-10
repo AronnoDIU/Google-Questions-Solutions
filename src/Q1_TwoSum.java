7835262519362058513d11628465d173dc4f14ea
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-11-11 00:13:06 +0600

    Q1_TwoSum

--- src/Q1_TwoSum.java
+++ src/Q1_TwoSum.java
@@ -7,8 +7,9 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].*/
 public class Q1_TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) { // Time Complexity: O(n^2)
         int[] result = new int[2];
+
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
